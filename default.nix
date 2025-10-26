116f96077653183e1760d82a7faf050fa67556e1
Kerstin Humm <kerstin@erictapen.name>
2025-10-26 13:37:07 +0100

    refactor nix expression for building web

---
+++ default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  rustPlatform,
+  trunk,
+  rustc,
+  wasm-bindgen-cli,
+  binaryen,
+  ibm-plex,
+  publicUrl ? "/",
+}:
+rustPlatform.buildRustPackage {
+  pname = "implementation";
+  version = "0.1.0";
+  src = ./.;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "ecolor-0.31.1" = "sha256-8ITdHxuDcDO1NL07luNQX3/7G3+5MmelYklVkqtGOFc=";
+      "fontique-0.4.0" = "sha256-4Be9ytfKCUwrUVmLtyp0quLgk3aIOLvP3zXAf+pFWuU=";
+      "mt940-1.0.2" = "sha256-IoNr7yQau9ExCo7RaneyzZ3vZYMex1cpU8HaQ7gbBSc=";
+      "swash-0.2.2" = "sha256-ZNsNFI3vKw7Yz+1aaGnmFK4lwS4tbw7F6oaGIw9cpJY=";
+      "zeno-0.3.2" = "sha256-41ocDmCTHa4ov6ygJRqF1metrXmlqB7Ekj9FLno5SXU=";
+    };
+  };
+
+  env.TRUNK_BUILD_PUBLIC_URL = publicUrl;
+
+  nativeBuildInputs = [
+    trunk
+    rustc.llvmPackages.lld
+    wasm-bindgen-cli
+    binaryen
+  ];
+
+  buildPhase = ''
+    mkdir -p assets/fonts
+    cp ${ibm-plex}/share/fonts/opentype/* assets/fonts/
+    trunk build \
+      --offline \
+      --frozen \
+      --release \
+      --dist $out
+  '';
+  installPhase = "echo 'Skipping installPhase'";
+  checkPhase = "echo 'Skipping installPhase'";
+}
