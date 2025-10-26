8ed474dcf5b4908473c67ca090da46509acf7874
Kerstin Humm <kerstin@erictapen.name>
2025-10-26 14:05:28 +0100

    add loading spinner from eframe template

--- src/main.rs
+++ src/main.rs
@@ -29,25 +29,39 @@ fn main() {
 
     log::info!("Starting app");
 
-    let document = web_sys::window()
-        .expect("No window")
-        .document()
-        .expect("No document");
-
-    let canvas = document
-        .get_element_by_id("the_canvas_id")
-        .expect("Failed to find the_canvas_id")
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .expect("the_canvas_id was not a HtmlCanvasElement");
-
     wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
             .start(
                 canvas,
                 web_options,
                 Box::new(|cc| Ok(Box::new(App::new(cc)))),
             )
-            .await
-            .expect("failed to start eframe");
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
     });
 }
